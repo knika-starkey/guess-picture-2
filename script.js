0d6ae97be1c2d8d288f5d151582ba7c365bb642e
knika-starkey <knika787@gmail.com>
2022-12-03 20:11:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// window.onload = function () {
+//   let image1 = document.getElementById("one");
+//   image1.onclick = showImageOne;
+//   let image2 = document.getElementById("two");
+//   image2.onclick = showImageTwo;
+// };
+
+// function showImageOne() {
+//   let image = document.getElementById("one");
+//   image.src = "images/one.jpg";
+// }
+
+// function showImageTwo() {
+//   let image = document.getElementById("two");
+//   image.src = "images/two.jpg";
+// }
+window.onload = function () {
+  let images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
+};
+function showBlur(image) {
+  let name = image.id;
+  image.src = `images/${name}blur.jpg`;
+}
+
+function showAnswer(event) {
+  let image = event.target;
+  let name = image.id;
+  name = name + ".jpg";
+  image.src = "images/" + name;
+  setTimeout(showBlur, 2000, image);
+}
